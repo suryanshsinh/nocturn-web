b97713cf576f7ef7d7cb00a3d35276933c75776c
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-02-02 11:13:26 +0530

    improve home page UI and add new font

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import *
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ static/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const cryptoList = document.getElementById('crypto-list');
+    cryptoList.lastElementChild.style.marginBottom = "20px";
+})
+
+const detail = () => {
+    window.location.href = '/detail';
+}
+
